f8f8190447fe44a191c0e905c09b8a2eb2cc175e
AlanTurist <59668425+AlanTurist@users.noreply.github.com>
2021-01-31 15:00:23 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import def_iter
+import def_stampa
+
+n = int(input("\nNumero di ripetizioni: "))
+print("\n")
+print("*"*50)
+def_stampa.stampa(n)
+print("\n")
+print("*"*50)
+print("\n\nIl risultato è:",def_iter.iter(n))
+print("\n")
